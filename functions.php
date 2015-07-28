df479bad31c3db151fe77ec7140ba0dc948f3c80
Jasen Peterson <jpeterson@fuelfx.com>
2015-07-28 17:29:45 -0500

    Additional File Updates

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+
+//Scripts & Styles
+
+function theme_scripts() {
+    
+    // Add Bootstrap default CSS
+    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/inc/css/bootstrap.min.css' );
+
+    // Add Font Awesome stylesheet
+    wp_enqueue_style( 'fontawesome', get_template_directory_uri().'/inc/css/font-awesome.min.css' );
+    
+    // Add Bootstrap default JS
+    wp_enqueue_script('bootstrapjs', get_template_directory_uri().'/inc/js/bootstrap.min.js', array('jquery') );
+    
+    // Add Theme JS
+    wp_enqueue_script('themejs', get_template_directory_uri().'/inc/js/theme.js', array('jquery') );
+
+}
+add_action( 'wp_enqueue_scripts', 'theme_scripts' );
---
+++ header.php
@@ -0,0 +1,71 @@
+<?php
+
+    //Header
+
+?>
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+   
+    <title>
+        <?php 
+            if ( function_exists('is_tag') && is_tag() ) 
+            
+            { 
+                
+                echo 'Tag Archive for &quot;'.$tag.'&quot; - '; 
+            
+            } 
+
+            elseif ( is_archive() ) 
+            
+            { 
+                
+                wp_title(''); echo ' Archive - '; 
+            
+            } 
+
+            elseif ( is_search() ) 
+            
+            { 
+                
+                echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; 
+            
+            } 
+            
+            elseif ( !(is_404() ) && ( is_single() ) || ( is_page() ) ) { 
+                
+                wp_title(''); echo ' - '; 
+            
+            } 
+            
+            elseif ( is_404() ) 
+            
+            { 
+                
+                echo 'Not Found - '; 
+            
+            } 
+
+            bloginfo('name'); 
+        ?>
+    </title>
+    
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+    <?php wp_head(); ?>
+
+</head>
+
+<body>
+    
